5f5bac9ced4a16a797c65c33b558bd4a392af2a2
David Poblador i Garcia <dpoblador@spotify.com>
2016-07-01 23:30:14 +0200

    Fix vagrant provisioning and nginx configuration

--- bootstrap-vagrant.sh
+++ bootstrap-vagrant.sh
@@ -6,3 +6,7 @@ if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant/public_html /var/www
 fi
+
+rm /etc/nginx/sites-available/default
+ln -s /vagrant/nginx-default-conf /etc/nginx/sites-available/default
+/etc/init.d/nginx restart
