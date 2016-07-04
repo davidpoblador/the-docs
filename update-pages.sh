b2ecf412a715f65c0400ebc75ba4f64f13f4459a
David Poblador i Garcia <dpoblador@spotify.com>
2016-07-04 13:02:25 +0200

    Fix publishing

--- update-pages.sh
+++ update-pages.sh
@@ -2,11 +2,17 @@
 
 ts=$(date +"%F %T")
 
-rsync -a --delete public_html/ dist/
-cd dist
+cd ../the-docs-publish
+git checkout gh-pages
 git pull
+cd -
+
+rsync -a --delete --exclude .git public_html/ ../the-docs-publish/
+
+cd ../the-docs-publish
 git add --all
 git commit -m "Update pages to new version ${ts}"
 git push
-cd ..
+cd -
+
 git pull
