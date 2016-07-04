bffea713a6f6fa783234582e4e90ebf8f289c115
David Poblador i Garcia <dpoblador@spotify.com>
2016-07-04 11:40:53 +0200

    Include timestamp in deployments commit messages

--- update-pages.sh
+++ update-pages.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+ts=$(date +"%F %T")
 rsync -a public_html/ dist/
 cd dist
 git pull
 git add --all
-git commit -m 'Update pages to new version'
+git commit -m "Update pages to new version %{ts}"
 git push
 cd ..
 git pull
