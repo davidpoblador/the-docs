6c9014d6d31f6b1131cc2f81b9276d37325deae7
David Poblador i Garcia <dpoblador@spotify.com>
2016-07-05 23:57:08 +0200

    Auto PEP-8

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from utils.fetcher import DebianManpageFetcher
 
 manpage_directory = "src/man-pages"
 
+
 def main():
     parser = dirparse.ManDirectoryParser(manpage_directory)
     parser.parse_directory()
 
     packages_to_ignore = set()
     for page, _ in parser.get_missing_links():
-        fetcher = DebianManpageFetcher(page, packages_to_ignore = packages_to_ignore)
+        fetcher = DebianManpageFetcher(
+            page, packages_to_ignore=packages_to_ignore)
         if fetcher.fetch():
             packages_to_ignore.add(fetcher.package)
 
