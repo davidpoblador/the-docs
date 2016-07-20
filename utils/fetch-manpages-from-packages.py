49be261cfb1d69acf65e3d47376e359c3d279e00
David Poblador i Garcia <dpoblador@spotify.com>
2016-07-20 17:58:16 +0200

    First iteration of python package fetcher

--- utils/fetch-manpages-from-packages.py
+++ utils/fetch-manpages-from-packages.py
@@ -17,19 +17,26 @@ dest_dir = pjoin(package_directory, "..", "src")
 def main():
     packages = dict()
     packages['acl'] = ("git://git.savannah.nongnu.org/acl.git", )
+    packages['man-pages'] = ("http://git.kernel.org/pub/scm/docs/man-pages/man-pages", )
 
     for package in packages:
+        logging.info("Processing package %s", package)
         package_dir = os.path.relpath(pjoin(output_dir, package))
         if os.path.isdir(package_dir):
             repo = git.Repo(package_dir)
             #repo.remotes[0].pull()
         else:
-            repo = git.Repo.clone_from(packages[package][0], package_dir, depth=1)
+            url = packages[package][0]
+            logging.info("%s not found, cloning from %s", package, url)
+
+            repo = git.Repo.clone_from(url, package_dir, depth=1)
 
 
         manpages = defaultdict(set)
-        for (dirpath, dirnames, filenames) in os.walk(package_dir):
+        for (dirpath, dirnames, filenames) in os.walk(package_dir, topdown = True):
             basename = os.path.basename(dirpath)
+            if '.git' in dirnames:
+                dirnames.remove('.git')
 
             for filename in filenames:
                 if '.' in filename:
@@ -41,7 +48,9 @@ def main():
                     section = ext[0]
                     if section in numbers:
                         file = pjoin(dirpath, filename)
-                        file_type = magic.from_file(file)
+                        ft = magic.Magic(keep_going = True)
+                        file_type = ft.from_file(file)
+                        print "DEBUG", filename, file, file_type
                         if "troff" in file_type:
                             non_numeric_section = "man%s" % (section, )
                             manpages[non_numeric_section].add(file)
